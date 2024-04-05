78dc078dc01807e23b2043855552abb105eb2ba2
julianfcp <julian.fcp@gmail.com>
2024-04-05 13:02:10 -0500

    structuring

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 import config from 'config';
 import express from "express";
-import logger from "./logger.js";
+import logger from "./middlewares/logger.js";
 import helmet from "helmet";
 import morgan from "morgan";
 import debug from 'debug';
+import router from './routes/movies.js';
 
 const app = express();
 
 //Configuration
 console.log("config: ", config.get('name'))
 console.log("config: ", config.get('mail.host'))
-console.log("config: ", config.get('mail.password'))
+//console.log("config: ", config.get('mail.password'))
+
+//templating engine
+app.set('view engine', 'pug');
+app.set('views', './views') // default
 
 //Debug  - set export DEBUG=app:startup to debug
 const startupDebugger = debug('app:startup');
@@ -20,7 +25,6 @@ startupDebugger(`Debugging here....`);
 // needs to export <variable>=<valor> on terminal
 console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
 console.log(`app: ${app.get('env')}`);
-
 
 if(app.get('env') === 'development') {
     app.use(morgan('tiny'));
@@ -32,24 +36,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); //key=value&key=value => req.body
 app.use(express.static('public'));
 app.use(helmet());
-
-
+//Routes
+app.use('/', router)
 
 //custom middlewares
 app.use(logger);
-
-//Data
-const genres = [
-    { id: '1', name: "Drama"},
-    { id: '2', name: "Thriller"},
-    { id: '3', name: "Action"},
-]
-
-//Routes
-app.get('/api/genres', (req, res) => {
-    res.send(genres)
-})
-
 
 //createServer
 const port = process.env.PORT || 3000;
